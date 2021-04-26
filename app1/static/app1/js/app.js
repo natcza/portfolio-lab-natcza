de964fe711b630a0ac5cecd4670eccc854353644
natcza <natalia.nc.czajkowska@gmail.com>
2021-04-26 23:44:43 +0200

    26.04

---
+++ app1/static/app1/js/app.js
@@ -0,0 +1,315 @@
+document.addEventListener("DOMContentLoaded", function() {
+  /**
+   * HomePage - Help section
+   */
+  class Help {
+    constructor($el) {
+      this.$el = $el;
+      this.$buttonsContainer = $el.querySelector(".help--buttons");
+      this.$slidesContainers = $el.querySelectorAll(".help--slides");
+      this.currentSlide = this.$buttonsContainer.querySelector(".active").parentElement.dataset.id;
+      this.init();
+    }
+
+    init() {
+      this.events();
+    }
+
+    events() {
+      /**
+       * Slide buttons
+       */
+      this.$buttonsContainer.addEventListener("click", e => {
+        if (e.target.classList.contains("btn")) {
+          this.changeSlide(e);
+        }
+      });
+
+      /**
+       * Pagination buttons
+       */
+      this.$el.addEventListener("click", e => {
+        if (e.target.classList.contains("btn") && e.target.parentElement.parentElement.classList.contains("help--slides-pagination")) {
+          this.changePage(e);
+        }
+      });
+    }
+
+    changeSlide(e) {
+      e.preventDefault();
+      const $btn = e.target;
+
+      // Buttons Active class change
+      [...this.$buttonsContainer.children].forEach(btn => btn.firstElementChild.classList.remove("active"));
+      $btn.classList.add("active");
+
+      // Current slide
+      this.currentSlide = $btn.parentElement.dataset.id;
+
+      // Slides active class change
+      this.$slidesContainers.forEach(el => {
+        el.classList.remove("active");
+
+        if (el.dataset.id === this.currentSlide) {
+          el.classList.add("active");
+        }
+      });
+    }
+
+    /**
+     * TODO: callback to page change event
+     */
+    changePage(e) {
+      e.preventDefault();
+      const page = e.target.dataset.page;
+
+      console.log(page);
+    }
+  }
+  const helpSection = document.querySelector(".help");
+  if (helpSection !== null) {
+    new Help(helpSection);
+  }
+
+  /**
+   * Form Select
+   */
+  class FormSelect {
+    constructor($el) {
+      this.$el = $el;
+      this.options = [...$el.children];
+      this.init();
+    }
+
+    init() {
+      this.createElements();
+      this.addEvents();
+      this.$el.parentElement.removeChild(this.$el);
+    }
+
+    createElements() {
+      // Input for value
+      this.valueInput = document.createElement("input");
+      this.valueInput.type = "text";
+      this.valueInput.name = this.$el.name;
+
+      // Dropdown container
+      this.dropdown = document.createElement("div");
+      this.dropdown.classList.add("dropdown");
+
+      // List container
+      this.ul = document.createElement("ul");
+
+      // All list options
+      this.options.forEach((el, i) => {
+        const li = document.createElement("li");
+        li.dataset.value = el.value;
+        li.innerText = el.innerText;
+
+        if (i === 0) {
+          // First clickable option
+          this.current = document.createElement("div");
+          this.current.innerText = el.innerText;
+          this.dropdown.appendChild(this.current);
+          this.valueInput.value = el.value;
+          li.classList.add("selected");
+        }
+
+        this.ul.appendChild(li);
+      });
+
+      this.dropdown.appendChild(this.ul);
+      this.dropdown.appendChild(this.valueInput);
+      this.$el.parentElement.appendChild(this.dropdown);
+    }
+
+    addEvents() {
+      this.dropdown.addEventListener("click", e => {
+        const target = e.target;
+        this.dropdown.classList.toggle("selecting");
+
+        // Save new value only when clicked on li
+        if (target.tagName === "LI") {
+          this.valueInput.value = target.dataset.value;
+          this.current.innerText = target.innerText;
+        }
+      });
+    }
+  }
+  document.querySelectorAll(".form-group--dropdown select").forEach(el => {
+    new FormSelect(el);
+  });
+
+  /**
+   * Hide elements when clicked on document
+   */
+  document.addEventListener("click", function(e) {
+    const target = e.target;
+    const tagName = target.tagName;
+
+    if (target.classList.contains("dropdown")) return false;
+
+    if (tagName === "LI" && target.parentElement.parentElement.classList.contains("dropdown")) {
+      return false;
+    }
+
+    if (tagName === "DIV" && target.parentElement.classList.contains("dropdown")) {
+      return false;
+    }
+
+    document.querySelectorAll(".form-group--dropdown .dropdown").forEach(el => {
+      el.classList.remove("selecting");
+    });
+  });
+
+  /**
+   * Switching between form steps
+   */
+  class FormSteps {
+    constructor(form) {
+      this.$form = form;
+      this.$next = form.querySelectorAll(".next-step");
+      this.$prev = form.querySelectorAll(".prev-step");
+      this.$step = form.querySelector(".form--steps-counter span");
+      this.currentStep = 1;
+
+      this.$stepInstructions = form.querySelectorAll(".form--steps-instructions p");
+      const $stepForms = form.querySelectorAll("form > div");
+      this.slides = [...this.$stepInstructions, ...$stepForms];
+
+      this.init();
+    }
+
+    /**
+     * Init all methods
+     */
+    init() {
+      this.events();
+      this.updateForm();
+    }
+
+    /**
+     * All events that are happening in form
+     */
+    events() {
+      // Next step
+      this.$next.forEach(btn => {
+        btn.addEventListener("click", e => {
+          e.preventDefault();
+          this.currentStep++;
+          this.updateForm();
+        });
+      });
+
+      // Previous step
+      this.$prev.forEach(btn => {
+        btn.addEventListener("click", e => {
+          e.preventDefault();
+          this.currentStep--;
+          this.updateForm();
+        });
+      });
+
+      // Form submit
+      this.$form.querySelector("form").addEventListener("submit", e => {
+        console.log(e)
+        this.submit(e)
+      });
+
+    }
+
+    /**
+     * Update form front-end
+     * Show next or previous section etc.
+     */
+    updateForm() {
+      this.$step.innerText = this.currentStep;
+
+      // TODO: Validation
+
+      this.slides.forEach(slide => {
+        slide.classList.remove("active");
+
+        if (slide.dataset.step == this.currentStep) {
+          slide.classList.add("active");
+        }
+      });
+
+      this.$stepInstructions[0].parentElement.parentElement.hidden = this.currentStep >= 6;
+      this.$step.parentElement.hidden = this.currentStep >= 6;
+
+      // TODO: get data from inputs and show them in summary
+      //
+      // let number_bags = document.getElementsByName('bags')
+      // let radio_button = document.querySelectorAll('.form-group--checkbox input[type="radio"]:checked')[0].value;
+      let test = document.querySelectorAll('.form-group--checkbox input[type="radio"]:checked')[0]
+      let radio_button_name =""
+      if (test !== undefined){
+        radio_button_name = test.parentElement.querySelector('.title').innerText;
+      }
+
+
+      // ------------------------------------
+      let inputs_fsc = document.querySelectorAll('.form-section--column');
+
+      let input_adres = inputs_fsc[0].children[1].children[0].control.value;
+      let input_miasto = inputs_fsc[0].children[2].children[0].control.value;
+      let input_kodpocztowy = inputs_fsc[0].children[3].children[0].control.value;
+      let input_nrtelefonu = inputs_fsc[0].children[4].children[0].control.value;
+
+      let input_date = inputs_fsc[1].children[1].children[0].children[0].value;
+      let input_godzina = inputs_fsc[1].children[2].children[0].children[0].value;
+      let input_uwaga = inputs_fsc[1].children[3].children[0].children[0].value;
+
+      // let number_bags = document.querySelector('.form-group.form-group--inline').lastElementChild.lastElementChild.value
+      let number_bags = document.querySelector('.form-group.form-group--inline').children[0].children[0].value;
+      const w_li = document.querySelector('.summary').querySelector('.form-section').querySelectorAll('li');
+      w_li[0].querySelector('.summary--text').innerText = number_bags + " worki ubrań w dobrym stanie dla dzieci";
+      w_li[1].querySelector('.summary--text').innerText = "Dla " + radio_button_name;
+      let innerText_summary_child = document.querySelector('.summary').children[1];
+      innerText_summary_child.children[0].children[1].children[0].innerText = input_adres;
+      innerText_summary_child.children[0].children[1].children[1].innerText = input_miasto;
+
+      innerText_summary_child.children[0].children[1].children[2].innerText = input_kodpocztowy;
+      innerText_summary_child.children[0].children[1].children[3].innerText = input_nrtelefonu;
+
+      innerText_summary_child.children[1].children[1].children[0].innerText = input_date;
+      innerText_summary_child.children[1].children[1].children[1].innerText = input_godzina;
+      innerText_summary_child.children[1].children[1].children[2].innerText = input_uwaga;
+
+    }
+
+    /**
+     * Submit form
+     *
+     * TODO: validation, send data to server
+     */
+    submit(e) {
+      e.preventDefault();
+
+      const formData = new FormData(this.$form.getElementsByTagName('form').item(0));
+      console.log(this.$form.getElementsByTagName('form'));
+      console.log('++++');
+
+      fetch('/add_donation/', {
+        method: 'post',
+        body: formData
+      }).then(function (response) {
+        window.location.href = response.url
+        return response.text();
+      }).then(function (data) {
+        console.log(data);
+      }).catch(function (error) {
+        console.error(error);
+      })
+
+
+      this.currentStep++;
+      this.updateForm();
+    }
+  }
+  const form = document.querySelector(".form--steps");
+  if (form !== null) {
+    new FormSteps(form);
+  }
+});
